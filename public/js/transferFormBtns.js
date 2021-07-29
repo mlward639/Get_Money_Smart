11ad8e5126c2a6ebac045c4c49d62d5160d4a91d
Maggie Ward <mlward639@gmail.com>
2021-07-29 15:06:08 -0400

    depostmoney.handlebars and associated CSS, depositFormBtns.js; still needs final testing

--- public/js/transferFormBtns.js
+++ public/js/transferFormBtns.js
@@ -2,33 +2,41 @@
 const transferFormHandler = async (event) => {
   event.preventDefault();
   // Collect values from transfer money form
-  const transferFrom = document.querySelector(".transferFrom").value.trim();
+  const transferFrom = document.querySelector('.transferFrom').value.trim();
   //console.log("transfer from", transferFrom);
-  const transferTo = document.querySelector(".transferTo").value.trim();
+  const transferTo = document.querySelector('.transferTo').value.trim();
   //console.log("transfer to", transferTo);
-  const transferAmount = document.querySelector(".transferAmt").value.trim();
+  const transferAmount = document.querySelector('.transferAmt').value.trim();
   //console.log("transfer amt", transferAmount);
   const transferComment = document
-    .querySelector(".transferComment")
+    .querySelector('.transferComment')
     .value.trim();
   //console.log("transfer cmt", transferComment);
 
   // Send POST request to API endpoint if required fields are filled out
   if (transferFrom && transferTo && transferAmount) {
     //NOT SURE ABOUT ROUTE AND PUT vs POST HERE (seems like put would completely override, i think want post to send the info and let backend manipulate it)
-    const response = await fetch("/api/transfermoney", {
-      method: "POST",
+    const response = await fetch('/api/transfermoney', {
+      method: 'POST',
       body: JSON.stringify({
         transferFrom,
         transferTo,
         transferAmount,
         transferComment,
       }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
     // if response goes through correctly, redirect browser to the dashboard page
     if (response.ok) {
-      document.location.assign("/dashboard");
+      let comment = '';
+      if (transferComment) {
+        comment = `Comment: ${transferComment}`;
+      } else {
+        comment = 'No comment entered.';
+      }
+      alert(`Congrats! You deposited $${transferAmount} from ${transferFrom} to ${transferTo}.
+      ${comment}`);
+      document.location.assign('/dashboard');
     } else {
       alert(response.statusText);
     }
@@ -36,5 +44,5 @@ const transferFormHandler = async (event) => {
 };
 
 document
-  .querySelector(".transferBtn")
-  .addEventListener("click", transferFormHandler);
+  .querySelector('.transferBtn')
+  .addEventListener('click', transferFormHandler);
