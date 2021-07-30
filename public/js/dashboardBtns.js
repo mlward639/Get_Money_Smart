447ca4b89d191db5077a4e1e02bdd7c698d282cb
Maggie Ward <mlward639@gmail.com>
2021-07-30 13:00:32 -0400

    history modal handlebar and associated CSS and JS in dashboardBtns.js. updated signup.handlebars to match model and login.js

--- public/js/dashboardBtns.js
+++ public/js/dashboardBtns.js
@@ -1,28 +1,30 @@
 // If click on the Deposit Money button, go to the /deposit URL
 function depositURL() {
-  document.location.assign("/deposit");
-};
+  document.location.assign('/deposit');
+}
 
-document.querySelector(".depositMoney").addEventListener("click", depositURL);
-
+document.querySelector('.depositMoney').addEventListener('click', depositURL);
 
 // If click on the Transfer Money button, go to the /transfer URL
 function transferURL() {
-  document.location.assign("/transfermoney");
-};
+  document.location.assign('/transfermoney');
+}
 
-document.querySelector(".transferMoney").addEventListener("click", transferURL);
+document.querySelector('.transferMoney').addEventListener('click', transferURL);
 
 // If click on the Make Payment button, go to the /transfer URL
-document.querySelector(".makePayment").addEventListener("click", transferURL);
-
+document.querySelector('.makePayment').addEventListener('click', transferURL);
 
 // If click on the Charge Card button, go to the Charge Card page
 function chargeCardURL() {
-  document.location.assign("/chargecard ");
-};
+  document.location.assign('/chargecard ');
+}
 
-document.querySelector(".chargeCard").addEventListener("click", chargeCardURL);
+document.querySelector('.chargeCard').addEventListener('click', chargeCardURL);
 
+// If click Clear Transaction History, clear the history by going to /clear URL
+function clearURL() {
+  document.location.assign('/clear');
+}
 
-
+document.querySelector('.clearBtn').addEventListener('click', clearURL); // WHY WONT THIS QUERY SELECTOR WORK! ONLY WORKS WITH ONCLICK=CLEARURL IN THE HTML FILE
